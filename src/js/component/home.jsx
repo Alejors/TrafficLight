b68318dfb678f947cf1faa7b60b4515b9d39bb0d
alejo <alejoatria@gmail.com>
2022-07-06 10:33:22 -0400

    agregado boton para luz extra. Boton toggle se deja para iterar por las luces onclick, no con intervalo

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -2,22 +2,12 @@ import React, { useState, useEffect } from "react";
 
 const Trafficlight = () => {
 	const [trafficColor, setTrafficColor] = useState("");
-
-	const [counter, setCounter] = useState('');
-
-	useEffect(() => {
-		if (counter !== '') {
-			let interval = setInterval(() => {
-				setCounter(counter + 1);
-				toggle();
-			}, 3000);
-		}
-	}
-	,[counter]);
+	const [show, setShow] = useState(false);
 
 	let toggle = () => {
-		(trafficColor === "") ? (setTrafficColor("red")) : (trafficColor === "red") ? (setTrafficColor("yellow")) : (trafficColor === "yellow") ? (setTrafficColor("green")) : (trafficColor === "green") ? (setTrafficColor("red")) : null;
+		(trafficColor === "") ? (setTrafficColor("red")) : (trafficColor === "red") ? (setTrafficColor("yellow")) : (trafficColor === "yellow") ? (setTrafficColor("green")) : (trafficColor === "green") ? (setTrafficColor("blue")) : (trafficColor === "blue") ? (setTrafficColor("red")) : null;
 	}
+
 	return (
 		<div className="container d-flex align-items-center flex-column">
 			<div className="bg-dark">" "</div>
@@ -25,10 +15,11 @@ const Trafficlight = () => {
 				<div onClick={() => setTrafficColor("red")} className={"rounded-circle bg-danger mt-1" + ((trafficColor !== "red") ? " bg-opacity-25" : "")}></div>
 				<div onClick={() => setTrafficColor("yellow")} className={"rounded-circle bg-warning mt-2" + ((trafficColor !== "yellow") ? " bg-opacity-25" : "")}></div>
 				<div onClick={() => setTrafficColor("green")} className={"rounded-circle bg-success mt-2" + ((trafficColor !== "green") ? " bg-opacity-25" : "")}></div>
+				<div onClick={() => setTrafficColor("blue")} className={"rounded-circle bg-primary mt-2" + ((trafficColor !== "blue") ? " bg-opacity-25" : "") + ((show === false) ? " d-none" : "")}></div>
 			</div>
 			<br />
-			<button type="button" onClick={(e) => setCounter(0)} className="btn btn-dark">Auto-toggle</button>
-			{/* <button type="button" className="btn btn-light mt-2">Add purple</button> */}
+			<button type="button" onClick={(e) => toggle()} className="btn btn-dark">Change me</button>
+			<button type="button" onClick={(e) => setShow(!show)} className="btn btn-light mt-2">Toggle extra light</button>
 
 		</div>
 	)
